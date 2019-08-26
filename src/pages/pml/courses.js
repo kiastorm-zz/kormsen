54a619f6e34aee41e89d0ddd3295553ddca3dfc7
Kia Storm <mrkiastorm@gmail.com>
2019-08-26 18:31:31 +1000

    more

--- src/pages/pml/courses.js
+++ src/pages/pml/courses.js
@@ -47,12 +47,14 @@ const Courses = (props) => {
 				<Sidebar />
 			</LeftCol>
 			<Box bg="lightGray" style={{height: '100%'}} p={8}>
-				<Heading color="red">I dont have time to do this page but some ideas to let you know: </Heading>
-				<Heading mt={4}>I am skilled in smooth animations, providing a great fluid user experience for your application/website</Heading>
-				<Heading mt={4}>Users will be able to filter products based on exactly what theyre looking for, if someone wants to buy a Serum tutorial, they can find only Serum tutorials by filtering via the sidebar</Heading>
-				<Heading mt={4}>Take note of the seamless transition between pages. This site (and your site would be) built in Gatsby JS. An amazingly fast static asset generator (your site optimises itself for the fastest possible experience) and all pages are preloading, meaning INSTANT load times <Link to="https://www.gatsbyjs.org/">Go here and check out some examples to learn more</Link> P.S:  Yes, it integrates with Shopify</Heading>
-				<Heading mt={4}>The homepage would be a Netflix style layout that expands upon clicking a product image. The homepage allows you to display 'Featured' products (proucts handpicked by you to display based on what you believe will sell) and also a right box showing the top rated/most popular products. Giving users multiple avenues to explore without even leaving the homepage</Heading>
-				<Heading mt={4}>I have plenty more ideas although I work full time and so didnt have much time to whip something up for you :) Hope you like</Heading>
+				<Heading color="red">I dont have time to do this page but some stuff to let you know: </Heading>
+				<ul>
+					<li><Heading mt={4}>I am skilled in smooth animations, providing a great fluid user experience for your application/website</Heading></li>
+					<li><Heading mt={4}>Users will be able to filter products based on exactly what theyre looking for, if someone wants to buy a Serum tutorial, they can find only Serum tutorials by filtering via the sidebar</Heading></li>
+					<li><Heading mt={4}>Take note of the seamless transition between pages. This site (and your site would be) built in Gatsby JS. An amazingly fast static asset generator (your site optimises itself for the fastest possible experience) and all pages are preloading, meaning INSTANT load times <Link to="https://www.gatsbyjs.org/">Go here and check out some examples to learn more</Link> P.S:  Yes, it integrates with Shopify</Heading></li>
+					<li><Heading mt={4}>The homepage would be a Netflix style layout that expands upon clicking a product image. The homepage allows you to display 'Featured' products (proucts handpicked by you to display based on what you believe will sell) and also a right box showing the top rated/most popular products. Giving users multiple avenues to explore without even leaving the homepage</Heading></li>
+					<li><Heading mt={4}>I have plenty more ideas although I work full time and so didnt have much time to whip something up for you :) Hope you like</Heading></li>
+				</ul>
 			</Box>
 		</PageContainer>
 	);
