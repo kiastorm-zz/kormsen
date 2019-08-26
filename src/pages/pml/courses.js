68bbaead0140814aa499bc2b680350d98251b20e
Kia Storm <mrkiastorm@gmail.com>
2019-08-26 10:50:19 +1000

    remove bullshit

--- src/pages/pml/courses.js
+++ src/pages/pml/courses.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import {Box, Grid, Flex, Heading, Card, Image} from 'rebass/styled-components';
 import Hero from '../../components/hero';
 import square1 from '../../assets/images/slide_5.webp';
-import Carousel from 'react-spring-3d-carousel';
 import randomString from '../../lib/utils/random-string';
 import PMLSidebar from "../../components/pml/pml-sidebar/pml-sidebar";
 import {animated, useTransition, useSpring, useChain} from 'react-spring';
